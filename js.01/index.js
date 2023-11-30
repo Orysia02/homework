417dc0a891131486be1c3772a597661acabbd526
Orysia02 <orysiamagon45@gmail.com>
2023-11-30 15:22:11 +0200

    work

---
+++ js.01/index.js
@@ -0,0 +1,7 @@
+let firstnumber = prompt('Введить число'); // зміннa1
+let secondnumber = prompt('Введіть число'); //змінна2
+
+let somePartialResult = firstParameter*5; //змінна і формула
+let someOtherPartialResult = secondParameter/100500; //
+
+let result = alert (somePartialResult + someOtherPartialResult); //результат і змінна
